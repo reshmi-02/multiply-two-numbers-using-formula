8fdb456361bece73260e661d0b1f17f9bb3cdaad
reshmi-02 <reshmihash123@gmail.com>
2024-01-06 16:18:05 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,6 @@
+let num1=parseInt(prompt("Enter the number"));
+let num2=parseInt(prompt("Enter the number"));
+document.write("Given num1 = "+num1+"<br><br>")
+document.write("Given num2 = "+num2+"<br><br>")
+let ans=(((num1+num2)**2)-(num1**2)-(num2**2))/2
+document.write("Answer = "+ans)
